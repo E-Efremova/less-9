972fe738509381314b5867dcbbb3532505ec517b
e.efremova <e.efremova@dextechnology.com>
2022-01-25 16:48:43 +0200

    12

---
+++ src/test/java/qa/efremova/SkippedTests.java
@@ -0,0 +1,21 @@
+package qa.efremova;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class SkippedTests {
+    @Test
+    @Disabled
+    void test00() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled("With some reason")
+    void test01() {
+        assertTrue(false);
+    }
+}
