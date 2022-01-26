d3c447ee4a56d2bb5e110a075727dcbb7d1cf86b
e.efremova <e.efremova@dextechnology.com>
2022-01-26 17:18:48 +0200

    addAttachments

--- src/test/java/demoqa/RegistrationTests.java
+++ src/test/java/demoqa/RegistrationTests.java
@@ -2,7 +2,9 @@ package demoqa;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -63,6 +65,14 @@ public class RegistrationTests {
         $(".table-responsive").shouldHave(text("NCR Gurgaon"));
 
 
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
     }
 
 }
